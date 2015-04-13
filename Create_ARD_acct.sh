a5d1817a3dd043c66b989ec3826cfb7f13f44408
Christopher Norris <ccnorris63@outlook.com>
2015-04-13 11:07:40 -0700

    Use jamf binary to setup tech and admin accounts
    Checks to see if the SSH access list exists, if not, it is created. 
    Checks to see if each account exists, if not, uses the jamf binary to create the account, home folder in /private/var, assign the same RealName, and add the account to the SSH access list
    Sets ARD permissions for access
    Displays the hidden accounts information

--- Create_ARD_acct.sh
+++ Create_ARD_acct.sh
@@ -1,75 +1,82 @@
 #!/bin/sh
 exec >> "/Library/Logs/ITSadmin Scripts.log" 2>&1
 
-SCRIPTNAME=$0
+# Last modified Apr. 13, 2015, Chris Norris
 
-# Last modified Nov. 12, 2013, Chris Norris
-
-echo "***************************** $SCRIPTNAME " 'date "+%A %m/%d/%Y %H:%M:%S"' " **********" 
+DATE=`date "+%A %m/%d/%Y %H:%M"`
+echo "********** $0 " `date "+%A %m/%d/%Y %H:%M:%S"` " **********" 
 
 # set variables
-build=`sw_vers | grep ProductVersion | cut -c 20`
-os=`sw_vers | grep ProductVersion | cut -c 17-20`
-ARD="/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart"
+Kickstart="/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart"
+UsersCreated=(itstech itsadmin)
+GenericPW='password'
 
-echo "Create user account folder structure…"
-mkdir -v /private/var/ITSadmin
+echo "=========== Create Administrative Accounts ============"
+# Enables Hide500Users to allow hiding accounts
+echo "enable hiding accounts from login and fast user switching menu…"
+if [[ $(defaults read /Library/Preferences/com.apple.loginwindow Hide500Users) != 1 ]]; then
+	echo "The Hide500Users option is Disabled. Changing to Enabled..."
+	defaults write /Library/Preferences/com.apple.loginwindow Hide500Users -bool TRUE
+else
+	echo "The Hide500Users option is already Enabled"
+fi
 
-echo "=========== Create ARD Account ============"
-dseditgroup -o edit -a ITSadmin -t user admin -P ITSadminpass -r ITSadmin
-dscl . -create /Users/ITSadmin NFSHomeDirectory /private/var/ITSadmin
-sleep 5
-chown -Rv ITSadmin /private/var/ITSadmin
-dscl . -create /Users/ITSadmin PrimaryGroupID 80
-dscl . -create /Users/ITSadmin UniqueID 405
-dscl . -create /Users/ITSadmin shell /bin/bash
+# Checks to see if the SSH access list exists, if not, it is created
+if [[ $(dseditgroup com.apple.access_ssh) = "Group not found." ]]; then
+	echo "The com.apple.access_ssh group does not exist, so it is being created." 
+	dseditgroup -o create com.apple.access_ssh
+else
+	echo "The com.apple.access_ssh group already exists."
+fi
 
-echo "Verify ITSadmin Account was created properly..."
-echo "list /Users | grep ITSadmin:"
-dscl . -list /Users | grep ITSadmin
+# Checks to see if each account exists, if not, uses the jamf binary to create the account,
+# home folder in /private/var, assign the same RealName, and add the account to the SSH access list
+for i in ${UsersCreated[@]}
+do
+    if [[ $(id -F "$i") = "$i" ]] 
+    then
+	    echo "The $i account already exists."
+    else
+	    /usr/sbin/jamf createAccount -username $i -realname $i -password $GenericPW -home /private/var/$i -hiddenUser -admin
+	    dseditgroup -o edit -a $i -t user com.apple.access_ssh
+    fi
+done
 
-echo "read  /Users/ITSadmin GeneratedUID:"
-dscl . -read /Users/ITSadmin GeneratedUID
+# sets the proper permissions for ARD access
+echo "Remote access will now be configured for proper Getty access"
+$Kickstart -activate
+$Kickstart -configure -allowAccessFor -specifiedUsers
+$Kickstart -configure -users itslabadmin,gettytech -access -on -privs -all -clientopts -setreqperm -reqperm yes
 
-echo "read /Users/ITSadmin realname:"
-dscl . -read /Users/ITSadmin realname
+# Displays the hidden accounts information
+for ACCT in ${UsersCreated[@]}
+do
+    echo "Is the $ACCT Account created?"
+    if [[ $(id -F "$ACCT") = "$ACCT" ) ]]
+	then
+		echo "The $ACCT account exists and has the following properties:"
+		echo "The account GeneratedUID (guid): \c"
+		dscl . -read /Users/$ACCT GeneratedUID | cut -c 15-
+		echo ""
+		echo "The account PrimaryGroupID: \c"
+		dscl . -read /Users/$ACCT PrimaryGroupID | cut -c 17-
+		echo ""
+		echo "The account UniqueID (uid): \c"
+		dscl . -read /Users/$ACCT UniqueID | cut -c 11-
+		echo ""
+		echo "The account Home Directory: \c"
+		dscl . -read /Users/$ACCT NFSHomeDirectory | cut -c 19-
+		echo ""
+		echo "This is the admin group membership:"
+		dscl . -read /Groups/admin GroupMembership | grep GroupMembership | cut -c 18-
+		echo ""
+		echo "This is the SSH access list:"
+		dscl /Local/Default read Groups/com.apple.access_ssh | grep GroupMembership | cut -c 18-
+	else
+		echo "<result>There in NO $ACCT Account on this system.</result>"
+	fi
+done
 
-echo "read /Users/ITSadmin NFSHomeDirectory:"
-dscl . -read /Users/ITSadmin NFSHomeDirectory
-
-echo "list /private/var | grep ITSadmin"
-ls -la /private/var | grep ITSadmin
-
-echo "read /Users/ITSadmin PrimaryGroupID:"
-dscl . -read /Users/ITSadmin PrimaryGroupID
-
-echo "read /Users/ITSadmin UniqueID:"
-dscl . -read /Users/ITSadmin UniqueID
-
-echo "read /Users/ITSadmin shell:"
-dscl . -read /Users/ITSadmin shell
-
-echo "Add ITSadmin account to the admin group..."
-dscl . -append /Groups/admin GroupMembership ITSadmin
-echo "Verify ARD Account was added to /Groups/admin..."
-dscl . -read /Groups/admin GroupMembership
-
-echo "hide ITSadmin account from login and fast user switching menu…"
-sudo defaults write /Library/Preferences/com.apple.loginwindow Hide500Users -bool TRUE
-sudo defaults write /Library/Preferences/com.apple.loginwindow HiddenUsersList -array add ard
-
-echo "Verify ITSadmin Account was hidden..."
-sudo defaults read /Library/Preferences/com.apple.loginwindow Hide500Users
-sudo defaults read /Library/Preferences/com.apple.loginwindow HiddenUsersList
-
-# different versions of the OS require different ARD commands
-echo "your OS is $os and ARD will now be configured for proper Getty access"
-$ARD -activate
-$ARD -configure -allowAccessFor -specifiedUsers
-$ARD -configure -users ITSadmin -access -on -privs -DeleteFiles -ControlObserve -TextMessages -OpenQuitApps -GenerateReports -RestartShutdown -SendFiles -ChangeSettings -clientopts -setreqperm -reqperm yes
-
-echo "The ITSadmin account has been created, ARD Service access set."
+echo "***************************** $0 Completed" `date "+%A %m/%d/%Y %H:%M:%S"` " **********" 
 
 exit 0
-
-
